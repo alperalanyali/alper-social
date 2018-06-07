0f24053e20ced2ebee8924ff16b93eb9aacda861
alperalanyali <alper@Alper-MacBook-Pro.local>
2018-06-07 15:32:32 +0300

    Completed setting up Firebase integreation

---
+++ alper-social/Controller/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  alper-social
+//
+//  Created by Alper on 7.06.2018.
+//  Copyright © 2018 Alper. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
