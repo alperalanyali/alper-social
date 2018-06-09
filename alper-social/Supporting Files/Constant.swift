3e5b9fb7beb8791dfe3675829f7e696e9b56dda9
alperalanyali <alper@Alper-MacBook-Pro.local>
2018-06-09 21:10:02 +0300

    Sign in ui custom classes completed

---
+++ alper-social/Supporting Files/Constant.swift
@@ -0,0 +1,9 @@
+//
+//  Constant.swift
+//  alper-social
+//
+//  Created by Alper on 7.06.2018.
+//  Copyright © 2018 Alper. All rights reserved.
+//
+
+import Foundation
---
+++ alper-social/View/RoundBtn.swift
@@ -0,0 +1,21 @@
+//
+//  RoundBtn.swift
+//  alper-social
+//
+//  Created by Alper on 7.06.2018.
+//  Copyright © 2018 Alper. All rights reserved.
+//
+
+import UIKit
+
+class RoundBtn: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
